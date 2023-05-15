1dd585823bc705716818e828a14f0bd789b0b505
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2023-05-15 20:59:54 +0200

    my first test commit

---
+++ src/main/java/pl/taw/EKlinikaApplication.java
@@ -0,0 +1,13 @@
+package pl.taw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKlinikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKlinikaApplication.class, args);
+	}
+
+}
